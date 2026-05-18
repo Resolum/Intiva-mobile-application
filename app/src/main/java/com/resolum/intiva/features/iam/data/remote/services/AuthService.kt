162433626b9913cb31d4f415ed0ae91bb4c6e9bd
Far14z <faridce14@gmail.com>
2026-05-18 14:20:29 -0500

    feat(sign-in): implement sign-in functionality with request and response handling in auth service and repository.

--- app/src/main/java/com/resolum/intiva/features/iam/data/remote/services/AuthService.kt
+++ app/src/main/java/com/resolum/intiva/features/iam/data/remote/services/AuthService.kt
@@ -1,5 +1,7 @@
 package com.resolum.intiva.features.iam.data.remote.services
 
+import com.resolum.intiva.features.iam.data.remote.models.SignInRequestDto
+import com.resolum.intiva.features.iam.data.remote.models.SignInResponseDto
 import com.resolum.intiva.features.iam.data.remote.models.SignUpRequestDto
 import com.resolum.intiva.features.iam.data.remote.models.SignUpResponseDto
 import retrofit2.Response
@@ -8,6 +10,7 @@ import retrofit2.http.POST
 
 /**
  * Retrofit service interface for authentication-related API calls.
+ * This interface defines the endpoints for user authentication, such as signing up a new user.
  */
 interface AuthService {
 
@@ -18,5 +21,14 @@ interface AuthService {
      * @return A [Response] containing the [SignUpResponseDto] if successful, or an error response if not.
      */
     @POST("authentication/sign-up")
-    suspend fun signUp(@Body body: SignUpRequestDto) : Response<SignUpResponseDto>
+    suspend fun signUp(@Body body: SignUpRequestDto) : SignUpResponseDto
+
+    /**
+     * Makes a POST request to the "authentication/sign-in" endpoint to authenticate an existing user.
+     *
+     * @param body The request body containing the user's sign-in information.
+     * @return A [Response] containing the [SignInResponseDto] if successful, or an error response if not.
+     */
+    @POST("authentication/sign-in")
+    suspend fun signIn(@Body body: SignInRequestDto) : SignInResponseDto
 }
