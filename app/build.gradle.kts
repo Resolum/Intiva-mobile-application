39aac0b091047c44b7e4f1fcd1a27c9429d536f3
Far14z <faridce14@gmail.com>
2026-05-29 16:31:18 -0500

    feat(build): add docker build configuration with docker base url setup.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,17 @@ android {
         debug {
             buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/v1/\"")
         }
+        create("docker") {
+            initWith(getByName("debug"))
+
+            buildConfigField(
+                "String",
+                "BASE_URL",
+                "\"http://10.0.2.2/api/v1/\""
+            )
+
+            matchingFallbacks += listOf("debug")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
