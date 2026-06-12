b5e8c7ba9ba14a5af2840e617aff14e278389877
Giovany7x <u202311601@upc.edu.pe>
2026-06-11 22:39:53 -0500

    fix(category): align create request dto

--- app/src/main/java/com/resolum/intiva/features/paymentmethodsandcategories/data/remote/models/CreateCategoryRequestDto.kt
+++ app/src/main/java/com/resolum/intiva/features/paymentmethodsandcategories/data/remote/models/CreateCategoryRequestDto.kt
@@ -4,9 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class CreateCategoryRequestDto(
     @SerializedName("name") val name: String,
-    @SerializedName("ownerType") val ownerType: String = "USER",
+    @SerializedName("ownerType") val ownerType: String,
+    @SerializedName("ownerId") val ownerId: Long,
     @SerializedName("description") val description: String = "",
     @SerializedName("color") val color: String = "#534AB7",
     @SerializedName("icon") val icon: String,
-    @SerializedName("isActive") val isActive: Boolean = true
+    @SerializedName("type") val type: String
 )
