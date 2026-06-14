9d683db20a0bfb9a3ac1e0a4feb724ae7be97c94
Giovany7x <u202311601@upc.edu.pe>
2026-06-14 11:28:38 -0500

    feat(sign-up): add terms navigation from sign up

--- app/src/main/java/com/resolum/intiva/core/root/AppRoot.kt
+++ app/src/main/java/com/resolum/intiva/core/root/AppRoot.kt
@@ -3,21 +3,39 @@ package com.resolum.intiva.core.root
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.resolum.intiva.core.ui.snackbar.IntivaSnackBarHost
 import com.resolum.intiva.features.iam.presentation.SignUpScreen
+import com.resolum.intiva.features.iam.presentation.TermsAndConditionsScreen
 
 /** The root composable of the app, setting up the main scaffold and navigation. */
 @Composable
 fun AppRoot() {
+    var showTermsAndConditions by remember { mutableStateOf(false) }
 
     Scaffold(
         snackbarHost = { IntivaSnackBarHost() }
     ) { padding ->
 
-        SignUpScreen(
-            modifier = Modifier.padding(padding)
-        ) {
+        if (showTermsAndConditions) {
+            TermsAndConditionsScreen(
+                onNavigateBack = {
+                    showTermsAndConditions = false
+                }
+            )
+        } else {
+            SignUpScreen(
+                modifier = Modifier.padding(padding),
+                showBackButton = false,
+                onNavigateToTermsAndConditions = {
+                    showTermsAndConditions = true
+                }
+            ) {
+            }
         }
     }
 }
