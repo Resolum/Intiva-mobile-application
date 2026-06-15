f7eaa4e0d0e7ca757fd6aaaf1e26fce3b302f119
Far14z <faridce14@gmail.com>
2026-06-15 16:30:17 -0500

    feat(deployment): update base url and network security configuration for production.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/v1/\"")
+            buildConfigField("String", "BASE_URL", "\"http://20.124.135.239/api/v1/\"")
         }
         create("docker") {
             initWith(getByName("debug"))
